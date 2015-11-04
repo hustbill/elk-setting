35b4c5178d053c6f9944f8d8c994a61b569baa32
Hua Zhang <billyzhang2010@gmail.com>
2015-11-03 18:19:00 -0800

    install golang by command

---
+++ README.md
@@ -0,0 +1,6 @@
+##Upstart configuration for CentOS 6 
+
+## Start Elasticsearch, logstash, kibana automatically
+
+
+##Install Logstash-forwarder on slave machines.
---
+++ install-golang.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl -O https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
